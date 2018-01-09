3c68e24d33e0660bc0a69e3da953eb2b19caf959
Isobel Redelmeier <iredelmeier@pivotal.io>
2018-01-09 10:10:40 -0800

    Use AccessContext::can_read_globally? in Access
    [#154155578]

--- app/access/base_access.rb
+++ app/access/base_access.rb
@@ -10,7 +10,7 @@ module VCAP::CloudController
 
     def read?(object)
       return @ok_read if instance_variable_defined?(:@ok_read)
-      @ok_read = (admin_user? || admin_read_only_user? || global_auditor? || object_is_visible_to_user?(object, context.user))
+      @ok_read = (context.can_read_globally? || object_is_visible_to_user?(object, context.user))
     end
 
     def read_for_update?(object, params=nil)
@@ -99,11 +99,6 @@ module VCAP::CloudController
     def admin_read_only_user?
       return @admin_read_only_user if instance_variable_defined?(:@admin_read_only_user)
       @admin_read_only_user = context.roles.admin_read_only?
-    end
-
-    def global_auditor?
-      return @global_auditor_user if instance_variable_defined?(:@global_auditor_user)
-      @global_auditor_user = context.roles.global_auditor?
     end
   end
 end
