2e772a5f6040c81b1603782b9ca27e165240b2fb
Isobel Redelmeier <iredelmeier@pivotal.io>
2018-02-09 14:19:00 -0800

    Extract permission name constants
    [#155014161]

--- lib/cloud_controller/perm/permissions.rb
+++ lib/cloud_controller/perm/permissions.rb
@@ -2,6 +2,15 @@ module VCAP
   module CloudController
     module Perm
       class Permissions
+        ORG_MANAGER_PERMISSION = 'org.manager'.freeze
+        ORG_AUDITOR_PERMISSION = 'org.auditor'.freeze
+        ORG_BILLING_MANAGER_PERMISSION = 'org.billing_manager'.freeze
+        ORG_USER_PERMISSION = 'org.user'.freeze
+
+        SPACE_MANAGER_PERMISSION = 'space.manager'.freeze
+        SPACE_DEVELOPER_PERMISSION = 'space.developer'.freeze
+        SPACE_AUDITOR_PERMISSION = 'space.auditor'.freeze
+
         def initialize(perm_client:, user_id:, issuer:, roles:)
           @perm_client = perm_client
           @user_id = user_id
@@ -26,17 +35,17 @@ module VCAP
 
         def can_read_from_org?(org_id)
           permissions = [
-            { permission_name: 'org.manager', resource_id: org_id },
-            { permission_name: 'org.auditor', resource_id: org_id },
-            { permission_name: 'org.user', resource_id: org_id },
-            { permission_name: 'org.billing_manager', resource_id: org_id },
+            { permission_name: ORG_MANAGER_PERMISSION, resource_id: org_id },
+            { permission_name: ORG_AUDITOR_PERMISSION, resource_id: org_id },
+            { permission_name: ORG_USER_PERMISSION, resource_id: org_id },
+            { permission_name: ORG_BILLING_MANAGER_PERMISSION, resource_id: org_id },
           ]
           can_read_globally? || has_any_permission?(permissions)
         end
 
         def can_write_to_org?(org_id)
           permissions = [
-            { permission_name: 'org.manager', resource_id: org_id },
+            { permission_name: ORG_MANAGER_PERMISSION, resource_id: org_id },
           ]
 
           can_write_globally? || has_any_permission?(permissions)
@@ -44,10 +53,10 @@ module VCAP
 
         def can_read_from_space?(space_id, org_id)
           permissions = [
-            { permission_name: 'space.developer', resource_id: space_id },
-            { permission_name: 'space.manager', resource_id: space_id },
-            { permission_name: 'space.auditor', resource_id: space_id },
-            { permission_name: 'org.manager', resource_id: org_id },
+            { permission_name: SPACE_DEVELOPER_PERMISSION, resource_id: space_id },
+            { permission_name: SPACE_MANAGER_PERMISSION, resource_id: space_id },
+            { permission_name: SPACE_AUDITOR_PERMISSION, resource_id: space_id },
+            { permission_name: ORG_MANAGER_PERMISSION, resource_id: org_id },
           ]
 
           can_read_globally? || has_any_permission?(permissions)
@@ -55,7 +64,7 @@ module VCAP
 
         def can_see_secrets_in_space?(space_id, org_id)
           permissions = [
-            { permission_name: 'space.developer', resource_id: space_id },
+            { permission_name: SPACE_DEVELOPER_PERMISSION, resource_id: space_id },
           ]
 
           can_read_secrets_globally? || has_any_permission?(permissions)
@@ -63,7 +72,7 @@ module VCAP
 
         def can_write_to_space?(space_id)
           permissions = [
-            { permission_name: 'space.developer', resource_id: space_id },
+            { permission_name: SPACE_DEVELOPER_PERMISSION, resource_id: space_id },
           ]
 
           can_write_globally? || has_any_permission?(permissions)
@@ -79,7 +88,12 @@ module VCAP
           perm_client.list_resource_patterns(
             user_id: user_id,
             issuer: issuer,
-            permissions: ['space.developer', 'space.manager', 'space.auditor', 'org.manager']
+            permissions: [
+              SPACE_DEVELOPER_PERMISSION,
+              SPACE_MANAGER_PERMISSION,
+              SPACE_AUDITOR_PERMISSION,
+              ORG_MANAGER_PERMISSION,
+            ]
           )
         end
 
