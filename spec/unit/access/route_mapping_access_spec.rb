ea3d8d0b5a690e2fe82c4f4abea7d4a7779ed739
Isobel Redelmeier <iredelmeier@gmail.com>
2018-02-21 16:54:21 -0800

    Make RouteMappingAccess tests action-based
    [#154225290]

--- spec/unit/access/route_mapping_access_spec.rb
+++ spec/unit/access/route_mapping_access_spec.rb
@@ -15,30 +15,141 @@ module VCAP::CloudController
 
     before { set_current_user(user, scopes: scopes) }
 
-    it_behaves_like :admin_read_only_access
+    describe 'when the org is suspended' do
+      before(:each) do
+        org.status = VCAP::CloudController::Organization::SUSPENDED
+        org.save
+      end
 
-    context 'admin' do
-      include_context :admin_setup
+      index_table = {
+        unauthenticated: true,
+        reader_and_writer: true,
+        reader: true,
+        writer: true,
 
-      it_behaves_like :full_access
+        admin: true,
+        admin_read_only: true,
+        global_auditor: true,
+
+        space_developer: true,
+        space_manager: true,
+        space_auditor: true,
+        org_user: true,
+        org_manager: true,
+        org_auditor: true,
+        org_billing_manager: true,
+      }
+
+      read_table = {
+        unauthenticated: false,
+        reader_and_writer: false,
+        reader: false,
+        writer: false,
+
+        admin: true,
+        admin_read_only: true,
+        global_auditor: true,
+
+        space_developer: true,
+        space_manager: true,
+        space_auditor: true,
+        org_user: false,
+        org_manager: true,
+        org_auditor: false,
+        org_billing_manager: false,
+      }
+
+      write_table = {
+        unauthenticated: false,
+        reader_and_writer: false,
+        reader: false,
+        writer: false,
+
+        admin: true,
+        admin_read_only: false,
+        global_auditor: false,
+
+        space_developer: false,
+        space_manager: false,
+        space_auditor: false,
+        org_user: false,
+        org_manager: false,
+        org_auditor: false,
+        org_billing_manager: false,
+      }
+
+      it_behaves_like('an access control', :create, write_table)
+      it_behaves_like('an access control', :delete, write_table)
+      it_behaves_like('an access control', :index, index_table)
+      it_behaves_like('an access control', :read, read_table)
+      it_behaves_like('an access control', :read_for_update, write_table)
+      it_behaves_like('an access control', :update, write_table)
     end
 
-    context 'space developer' do
-      before do
-        org.add_user(user)
-        space.add_developer(user)
-      end
+    describe 'when the org is not suspended' do
+      index_table = {
+        unauthenticated: true,
+        reader_and_writer: true,
+        reader: true,
+        writer: true,
 
-      it_behaves_like :full_access
+        admin: true,
+        admin_read_only: true,
+        global_auditor: true,
 
-      context 'when the organization is suspended' do
-        before do
-          org.status = VCAP::CloudController::Organization::SUSPENDED
-          org.save
-        end
+        space_developer: true,
+        space_manager: true,
+        space_auditor: true,
+        org_user: true,
+        org_manager: true,
+        org_auditor: true,
+        org_billing_manager: true,
+      }
 
-        it_behaves_like :read_only_access
-      end
+      read_table = {
+        unauthenticated: false,
+        reader_and_writer: false,
+        reader: false,
+        writer: false,
+
+        admin: true,
+        admin_read_only: true,
+        global_auditor: true,
+
+        space_developer: true,
+        space_manager: true,
+        space_auditor: true,
+        org_user: false,
+        org_manager: true,
+        org_auditor: false,
+        org_billing_manager: false,
+      }
+
+      write_table = {
+        unauthenticated: false,
+        reader_and_writer: false,
+        reader: false,
+        writer: false,
+
+        admin: true,
+        admin_read_only: false,
+        global_auditor: false,
+
+        space_developer: true,
+        space_manager: false,
+        space_auditor: false,
+        org_user: false,
+        org_manager: false,
+        org_auditor: false,
+        org_billing_manager: false,
+      }
+
+      it_behaves_like('an access control', :create, write_table)
+      it_behaves_like('an access control', :delete, write_table)
+      it_behaves_like('an access control', :index, index_table)
+      it_behaves_like('an access control', :read, read_table)
+      it_behaves_like('an access control', :read_for_update, write_table)
+      it_behaves_like('an access control', :update, write_table)
     end
   end
 end
