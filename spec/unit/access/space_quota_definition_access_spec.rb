929729c4c3b843b51b1dbf3ca22a3c81e2d16236
Isobel Redelmeier <iredelmeier@gmail.com>
2018-02-21 17:36:49 -0800

    Make SpaceQuotaDefinitionAccess tests action-based
    [#154225290]

--- spec/unit/access/space_quota_definition_access_spec.rb
+++ spec/unit/access/space_quota_definition_access_spec.rb
@@ -5,159 +5,236 @@ module VCAP::CloudController
     subject(:access) { SpaceQuotaDefinitionAccess.new(Security::AccessContext.new) }
     let(:user) { VCAP::CloudController::User.make }
     let(:org) { Organization.make }
-    let(:scopes) { nil }
     let(:space) { Space.make(organization: org) }
+    let(:scopes) { nil }
     let(:object) { VCAP::CloudController::SpaceQuotaDefinition.make(organization: org) }
 
     before { set_current_user(user, scopes: scopes) }
 
-    it_behaves_like :admin_full_access
-    it_behaves_like :admin_read_only_access
-    it_behaves_like :global_auditor_access
-
-    context 'organization manager' do
-      before { org.add_manager(user) }
-      it_behaves_like :full_access
-
-      context 'when the organization is suspended' do
-        let(:org) { Organization.make(status: Organization::SUSPENDED) }
-
-        it_behaves_like :read_only_access
+    describe 'when the organization is suspended' do
+      before(:each) do
+        org.status = VCAP::CloudController::Organization::SUSPENDED
+        org.save
       end
-    end
 
-    context 'when it is not applied to the space' do
-      context 'space manager' do
-        before do
-          org.add_user(user)
-          space.add_manager(user)
+      index_table = {
+        unauthenticated: true,
+        reader_and_writer: true,
+        reader: true,
+        writer: true,
+
+        admin: true,
+        admin_read_only: true,
+        global_auditor: true,
+
+        space_developer: true,
+        space_manager: true,
+        space_auditor: true,
+        org_user: true,
+        org_manager: true,
+        org_auditor: true,
+        org_billing_manager: true,
+      }
+
+      write_table = {
+        unauthenticated: false,
+        reader_and_writer: false,
+        reader: false,
+        writer: false,
+
+        admin: true,
+        admin_read_only: false,
+        global_auditor: false,
+
+        space_developer: false,
+        space_manager: false,
+        space_auditor: false,
+        org_user: false,
+        org_manager: false,
+        org_auditor: false,
+        org_billing_manager: false,
+      }
+
+      describe 'when the space is associated with the space quota definition' do
+        before(:each) do
+          space.space_quota_definition = object
+          space.save
         end
 
-        it_behaves_like :no_access
+        read_table = {
+          unauthenticated: false,
+          reader_and_writer: false,
+          reader: false,
+          writer: false,
+
+          admin: true,
+          admin_read_only: true,
+          global_auditor: true,
+
+          space_developer: true,
+          space_manager: true,
+          space_auditor: true,
+          org_user: false,
+          org_manager: true,
+          org_auditor: false,
+          org_billing_manager: false,
+        }
+
+        it_behaves_like('an access control', :create, write_table)
+        it_behaves_like('an access control', :delete, write_table)
+        it_behaves_like('an access control', :index, index_table)
+        it_behaves_like('an access control', :read, read_table)
+        it_behaves_like('an access control', :read_for_update, write_table)
+        it_behaves_like('an access control', :update, write_table)
       end
 
-      context 'space developer' do
-        before do
-          org.add_user(user)
-          space.add_developer(user)
+      describe 'when the space is not associated with the space quota definition' do
+        read_table = {
+          unauthenticated: false,
+          reader_and_writer: false,
+          reader: false,
+          writer: false,
+
+          admin: true,
+          admin_read_only: true,
+          global_auditor: true,
+
+          space_developer: false,
+          space_manager: false,
+          space_auditor: false,
+          org_user: false,
+          org_manager: true,
+          org_auditor: false,
+          org_billing_manager: false,
+        }
+
+        it_behaves_like('an access control', :create, write_table)
+        it_behaves_like('an access control', :delete, write_table)
+        it_behaves_like('an access control', :index, index_table)
+        it_behaves_like('an access control', :read, read_table)
+        it_behaves_like('an access control', :read_for_update, write_table)
+        it_behaves_like('an access control', :update, write_table)
+      end
+    end
+
+    describe 'when the organization is not suspended' do
+      index_table = {
+        unauthenticated: true,
+        reader_and_writer: true,
+        reader: true,
+        writer: true,
+
+        admin: true,
+        admin_read_only: true,
+        global_auditor: true,
+
+        space_developer: true,
+        space_manager: true,
+        space_auditor: true,
+        org_user: true,
+        org_manager: true,
+        org_auditor: true,
+        org_billing_manager: true,
+      }
+
+      read_table = {
+        unauthenticated: false,
+        reader_and_writer: false,
+        reader: false,
+        writer: false,
+
+        admin: true,
+        admin_read_only: true,
+        global_auditor: true,
+
+        space_developer: true,
+        space_manager: true,
+        space_auditor: true,
+        org_user: false,
+        org_manager: true,
+        org_auditor: false,
+        org_billing_manager: false,
+      }
+
+      write_table = {
+        unauthenticated: false,
+        reader_and_writer: false,
+        reader: false,
+        writer: false,
+
+        admin: true,
+        admin_read_only: false,
+        global_auditor: false,
+
+        space_developer: false,
+        space_manager: false,
+        space_auditor: false,
+        org_user: false,
+        org_manager: true,
+        org_auditor: false,
+        org_billing_manager: false,
+      }
+
+      describe 'when the space is associated with the space quota definition' do
+        before(:each) do
+          space.space_quota_definition = object
+          space.save
         end
 
-        it_behaves_like :no_access
+        read_table = {
+          unauthenticated: false,
+          reader_and_writer: false,
+          reader: false,
+          writer: false,
+
+          admin: true,
+          admin_read_only: true,
+          global_auditor: true,
+
+          space_developer: true,
+          space_manager: true,
+          space_auditor: true,
+          org_user: false,
+          org_manager: true,
+          org_auditor: false,
+          org_billing_manager: false,
+        }
+
+        it_behaves_like('an access control', :create, write_table)
+        it_behaves_like('an access control', :delete, write_table)
+        it_behaves_like('an access control', :index, index_table)
+        it_behaves_like('an access control', :read, read_table)
+        it_behaves_like('an access control', :read_for_update, write_table)
+        it_behaves_like('an access control', :update, write_table)
       end
 
-      context 'space auditor' do
-        before do
-          org.add_user(user)
-          space.add_auditor(user)
-        end
+      describe 'when the space is not associated with the space quota definition' do
+        read_table = {
+          unauthenticated: false,
+          reader_and_writer: false,
+          reader: false,
+          writer: false,
 
-        it_behaves_like :no_access
+          admin: true,
+          admin_read_only: true,
+          global_auditor: true,
+
+          space_developer: false,
+          space_manager: false,
+          space_auditor: false,
+          org_user: false,
+          org_manager: true,
+          org_auditor: false,
+          org_billing_manager: false,
+        }
+
+        it_behaves_like('an access control', :create, write_table)
+        it_behaves_like('an access control', :delete, write_table)
+        it_behaves_like('an access control', :index, index_table)
+        it_behaves_like('an access control', :read, read_table)
+        it_behaves_like('an access control', :read_for_update, write_table)
+        it_behaves_like('an access control', :update, write_table)
       end
-    end
-
-    context 'when it is applied to the space' do
-      before do
-        space.space_quota_definition = object
-        space.save
-      end
-
-      context 'space manager' do
-        before do
-          org.add_user(user)
-          space.add_manager(user)
-        end
-
-        it_behaves_like :read_only_access
-      end
-
-      context 'space developer' do
-        before do
-          org.add_user(user)
-          space.add_developer(user)
-        end
-
-        it_behaves_like :read_only_access
-      end
-
-      context 'space auditor' do
-        before do
-          org.add_user(user)
-          space.add_auditor(user)
-        end
-
-        it_behaves_like :read_only_access
-      end
-    end
-
-    context 'organization auditor (defensive)' do
-      before { org.add_auditor(user) }
-      it_behaves_like :no_access
-    end
-
-    context 'organization billing manager (defensive)' do
-      before { org.add_billing_manager(user) }
-      it_behaves_like :no_access
-    end
-
-    context 'organization user (defensive)' do
-      before { org.add_user(user) }
-      it_behaves_like :no_access
-    end
-
-    context 'user in a different organization (defensive)' do
-      before do
-        different_organization = VCAP::CloudController::Organization.make
-        different_organization.add_user(user)
-      end
-
-      it_behaves_like :no_access
-    end
-
-    context 'manager in a different organization (defensive)' do
-      before do
-        different_organization = VCAP::CloudController::Organization.make
-        different_organization.add_manager(user)
-      end
-
-      it_behaves_like :no_access
-    end
-
-    context 'a user that isnt logged in (defensive)' do
-      let(:user) { nil }
-
-      it_behaves_like :no_access
-    end
-
-    context 'any user using client without cloud_controller.write' do
-      let(:scopes) { ['cloud_controller.read'] }
-
-      before do
-        org.add_user(user)
-        org.add_manager(user)
-        org.add_billing_manager(user)
-        org.add_auditor(user)
-        space.add_manager(user)
-        space.add_developer(user)
-        space.add_auditor(user)
-      end
-
-      it_behaves_like :read_only_access
-    end
-
-    context 'any user using client without cloud_controller.read' do
-      let(:scopes) { [] }
-
-      before do
-        org.add_user(user)
-        org.add_manager(user)
-        org.add_billing_manager(user)
-        org.add_auditor(user)
-        space.add_manager(user)
-        space.add_developer(user)
-        space.add_auditor(user)
-      end
-
-      it_behaves_like :no_access
     end
   end
 end
