81ae53a1324dfba35966c377d9c287d5da673072
Isobel Redelmeier <iredelmeier@gmail.com>
2018-02-21 16:46:35 -0800

    Make SecurityGroupAccess tests action-based
    [#154225290]

--- spec/unit/access/security_group_access_spec.rb
+++ spec/unit/access/security_group_access_spec.rb
@@ -3,31 +3,75 @@ require 'spec_helper'
 module VCAP::CloudController
   RSpec.describe SecurityGroupAccess, type: :access do
     subject(:access) { SecurityGroupAccess.new(Security::AccessContext.new) }
-    let(:space) { Space.make }
+    let(:org) { Organization.make }
+    let(:space) { Space.make(organization: org) }
     let(:user) { User.make }
     let(:object) { SecurityGroup.make(space_guids: [space.guid]) }
 
     before { set_current_user(user) }
 
-    it_behaves_like :admin_read_only_access
+    index_table = {
+      unauthenticated: true,
+      reader_and_writer: true,
+      reader: true,
+      writer: true,
 
-    context 'admin' do
-      it_should_behave_like :admin_full_access
-    end
+      admin: true,
+      admin_read_only: true,
+      global_auditor: true,
 
-    context 'non admin' do
-      context 'when the user is a developer' do
-        before do
-          space.organization.add_user(user)
-          space.add_developer(user)
-        end
+      space_developer: true,
+      space_manager: true,
+      space_auditor: true,
+      org_user: true,
+      org_manager: true,
+      org_auditor: true,
+      org_billing_manager: true,
+    }
 
-        it_should_behave_like :read_only_access
-      end
+    read_table = {
+      unauthenticated: false,
+      reader_and_writer: false,
+      reader: false,
+      writer: false,
 
-      context 'when the user is not a developer of the owning space' do
-        it_should_behave_like :no_access
-      end
-    end
+      admin: true,
+      admin_read_only: true,
+      global_auditor: true,
+
+      space_developer: true,
+      space_manager: true,
+      space_auditor: true,
+      org_user: false,
+      org_manager: true,
+      org_auditor: false,
+      org_billing_manager: false,
+    }
+
+    write_table = {
+      unauthenticated: false,
+      reader_and_writer: false,
+      reader: false,
+      writer: false,
+
+      admin: true,
+      admin_read_only: false,
+      global_auditor: false,
+
+      space_developer: false,
+      space_manager: false,
+      space_auditor: false,
+      org_user: false,
+      org_manager: false,
+      org_auditor: false,
+      org_billing_manager: false,
+    }
+
+    it_behaves_like('an access control', :create, write_table)
+    it_behaves_like('an access control', :delete, write_table)
+    it_behaves_like('an access control', :index, index_table)
+    it_behaves_like('an access control', :read, read_table)
+    it_behaves_like('an access control', :read_for_update, write_table)
+    it_behaves_like('an access control', :update, write_table)
   end
 end
