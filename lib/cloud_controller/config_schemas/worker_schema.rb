bddb48242482be4b2cf80c8ccde3780c8fe1374e
Julian Skupnjak <skupnjak@de.ibm.com>
2018-08-24 17:47:11 +0300

    Implement OPI staging
    [#158463227]
    
    Signed-off-by: Julian Skupnjak <skupnjak@de.ibm.com>

--- lib/cloud_controller/config_schemas/worker_schema.rb
+++ lib/cloud_controller/config_schemas/worker_schema.rb
@@ -160,6 +160,7 @@ module VCAP::CloudController
           opi: {
             enabled: bool,
             url: String,
+            opi_staging: bool,
           },
 
           allow_app_ssh_access: bool,
