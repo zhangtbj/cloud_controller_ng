855e4321ca72ecf4a8718131cd467c486a03434b
Georgi Dankov <gddankov@gmail.com>
2018-08-31 18:41:42 +0300

    Translate StagingDetails to StagingRequest before sending it
    [#158463227]
    
    Signed-off-by: Mario Nitchev <marionitchev@gmail.com>

--- lib/cloud_controller/opi/stager_client.rb
+++ lib/cloud_controller/opi/stager_client.rb
@@ -5,12 +5,15 @@ require 'cloud_controller/opi/helpers'
 
 module OPI
   class StagerClient
-    def initialize(opi_url)
+    def initialize(opi_url, config)
       @client = HTTPClient.new(base_url: URI(opi_url))
+      @config = config
     end
 
     def stage(staging_guid, staging_details)
-      payload = MultiJson.dump(staging_details.message)
+      staging_request = to_request(staging_details)
+      payload = MultiJson.dump(staging_request.message)
+
       response = @client.post("/stage/#{staging_guid}", body: payload)
       if response.status_code != 200
         response_json = OPI.recursive_ostruct(JSON.parse(response.body))
@@ -19,5 +22,11 @@ module OPI
     end
 
     def stop_staging(staging_guid); end
+
+    private
+
+    def to_request(staging_details)
+      VCAP::CloudController::Diego::Protocol.new.stage_package_request(@config, staging_details)
+    end
   end
 end
